3d5f8dd2fc708558c7eca4bfcda20d00d0140e2a
Kavia AI <production@kavia.ai>
2025-06-27 03:59:10 +0000

    Auto-commit before merging to kavia-main from cga-cge80407d0

---
+++ resume_frontend_workspace/.linter.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /home/kavia/workspace/code-generation/slidecv-modern-markdown-resume-74344-8b909eeb/resume_frontend_workspace/resume_frontend
+npx run lint
+ESLINT_EXIT_CODE=$?
+npm run build
+BUILD_EXIT_CODE=$?
+if [ $ESLINT_EXIT_CODE -ne 0 ] || [ $BUILD_EXIT_CODE -ne 0 ]; then
+  exit 1
+fi
+
